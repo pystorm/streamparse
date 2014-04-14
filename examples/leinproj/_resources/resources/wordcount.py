0cbd8ef7185b85956d67cf06da3948f682cdc251
Keith Bourgoin <keith@parsely.com>
2014-04-14 10:48:27 -0400

    fix wordcount example to be mostly functional

--- examples/leinproj/_resources/resources/wordcount.py
+++ examples/leinproj/_resources/resources/wordcount.py
@@ -1,1 +1,17 @@
-print "wordcount"
+from collections import defaultdict
+from pystorm import storm
+from pystorm.storm import BasicBolt
+
+class WordCounter(BasicBolt):
+
+    def initialize(self, stormconf, context):
+        self.counts = defaultdict(int)
+
+    def process(self, tup):
+        word = tup.values[0]
+        self.counts[word] += 1
+        if self.counts[word] % 10 == 0:
+            storm.emit([word, self.counts[word]])
+            storm.log('%s: %d' % (word, self.counts[word]))
+
+WordCounter().run()
--- examples/leinproj/_resources/resources/words.py
+++ examples/leinproj/_resources/resources/words.py
@@ -1,1 +1,24 @@
-print "hi"
+import itertools
+
+from pystorm import storm
+from pystorm.storm import Spout
+
+class WordSpout(Spout):
+
+    def reader(self):
+        with open('./pg12.txt') as f:
+            for line in f.readlines():
+                words = line.lower().strip().split()
+                for word in words:
+                    if word:
+                        yield word
+
+    def initialize(self, stormconf, context):
+        #self.words = reader()
+        self.words = itertools.cycle(['a', 'b', 'c', 'd'])
+
+    def nextTuple(self):
+        word = next(self.words)
+        storm.emit([word,])
+
+WordSpout().run()
