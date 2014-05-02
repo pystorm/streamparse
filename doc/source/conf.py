4e2f757066b9644c3eea9b9fd4a117775d63e1eb
Andrew Montalenti <andrew@parsely.com>
2014-05-02 16:53:17 -0400

    More name changes in Sphinx.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# pystorm documentation build configuration file, created by
+# streamparse documentation build configuration file, created by
 # sphinx-quickstart on Sun Jan 19 22:29:52 2014.
 #
 # This file is execfile()d with the current directory set to its
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pystorm'
+project = u'streamparse'
 copyright = u'2014, Parsely'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -181,7 +181,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pystormdoc'
+htmlhelp_basename = 'streamparsedoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -201,7 +201,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'pystorm.tex', u'pystorm Documentation',
+  ('index', 'streamparse.tex', u'streamparse Documentation',
    u'Parsely', 'manual'),
 ]
 
@@ -231,7 +231,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'pystorm', u'pystorm Documentation',
+    ('index', 'streamparse', u'streamparse Documentation',
      [u'Parsely'], 1)
 ]
 
@@ -245,8 +245,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'pystorm', u'pystorm Documentation',
-   u'Parsely', 'pystorm', 'One line description of project.',
+  ('index', 'streamparse', u'streamparse Documentation',
+   u'Parsely', 'streamparse', 'Run Python on real-time streams of data.',
    'Miscellaneous'),
 ]
 
