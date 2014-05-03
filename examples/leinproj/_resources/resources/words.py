b2f6b22e92d531791b77ec68d6934e5dd653fba6
Andrew Montalenti <andrew@parsely.com>
2014-05-03 17:29:33 -0400

    fix

--- examples/leinproj/_resources/resources/words.py
+++ examples/leinproj/_resources/resources/words.py
@@ -5,7 +5,7 @@ from streamparse import storm
 class WordSpout(storm.Spout):
 
     def initialize(self, stormconf, context):
-        self.words = itertools.cycle(['dog', 'cat', 
+        self.words = itertools.cycle(['dog', 'cat',
                                       'zebra', 'elephant'])
 
     def nextTuple(self):
