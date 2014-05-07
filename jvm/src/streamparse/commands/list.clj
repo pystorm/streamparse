51ac698b9e5c94a5b4239fd87e9e92dba0f33108
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-05-07 16:22:26 -0400

    Add list and kill_topology commands, managed from a bash script. #7

---
+++ jvm/src/streamparse/commands/list.clj
@@ -0,0 +1,58 @@
+(ns streamparse.commands.list
+  "List topologies command for streamparse."
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [cli]]
+            [clojure.pprint :refer [print-table]]
+            [clojure.data.json :as json]
+            [backtype.storm.thrift :refer :all])
+  (:import [backtype.storm.generated TopologySummary])
+  (:gen-class))
+
+
+(defn topologies-as-map [hostname port]
+  "Fetch all topologies from a Nimbus cluster assumed to be running on
+  localhost and return as a vector of maps with topology information"
+  (with-nimbus-connection [nimbus hostname port]
+    (let [cluster-info (.getClusterInfo nimbus)
+          topologies (.get_topologies cluster-info)]
+      (vec (map #(hash-map :id (.get_id %)
+                           :name (.get_name %)
+                           :status (.get_status %)
+                           :num-workers (.get_num_workers %)
+                           :num-tasks (.get_num_tasks %)
+                           :num-executors (.get_num_executors %)
+                           :uptime-secs (.get_uptime_secs %)) topologies)))))
+
+
+(defn list-topologies [hostname port {:keys [as-json] :or {as-json false}}]
+  "List all running topologies for a Nimbus cluster assumed to be running on
+  localhost either as JSON or a pretty-printed table."
+  (let [topologies (topologies-as-map hostname port)]
+    (if (or (nil? topologies) (empty? topologies))
+      (println "No topologies running.")
+      (if as-json
+        (println (json/write-str topologies))
+        (print-table [:name :status :num-workers :num-executors :num-tasks :uptime-secs]
+                     topologies)))))
+
+
+(defn usage []
+  (->> ["sparsej list"
+        ""
+        "List topologies."
+        ""]
+        (string/join \newline)))
+
+
+(defn -main [& args]
+  (let [[opts args banner]
+         (cli args
+              ["-h" "--help" "Show this help screen." :flag true :default false]
+              ["-j" "--json" "Show output as JSON." :flag true :default false]
+              ["-n" "--host" "Hostname for Nimbus." :default "localhost"]
+              ["-p" "--port" "Port for Nimbus." :default 6627 :parse-fn #(Integer/parseInt %)])]
+    (when (:help opts)
+      (println (usage))
+      (println banner)
+      (System/exit 0))
+    (list-topologies (:host opts) (:port opts) {:as-json (:json opts)})))
