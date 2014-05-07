8d66e4da392c32abcfa3d0d3a089a1277e75759a
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-05-07 16:20:22 -0400

    Fix up project.clj. #7
    Add missing requirements and skip :main declaration since we'll just use a shell script to direct traffic.

--- jvm/project.clj
+++ jvm/project.clj
@@ -12,14 +12,15 @@
   :dependencies [[commons-collections/commons-collections "3.2.1"]]
   :profiles {:dev
               {:dependencies [[storm "0.8.2"]
+                              [storm/tools.cli "0.2.2"]
                               [org.clojure/clojure "1.4.0"]
+                              [org.clojure/tools.cli "0.3.1"]
+                              [org.clojure/data.json "0.2.4"]
                               [org.testng/testng "6.8.5"]
                               [org.easytesting/fest-assert-core "2.0M8"]
                               [org.mockito/mockito-all "1.9.0"]
                               [org.jmock/jmock "2.6.0"]]}}
-  :min-lein-version "2.0.0"
   :source-paths ["src"]
   :test-paths ["test"]
-  :aot :all
-  :main streamparse)
   :resource-paths ["resources"]
+  :aot :all)
