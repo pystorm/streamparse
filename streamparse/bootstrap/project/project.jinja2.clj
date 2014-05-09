8faa29110825d32531ca6e960e8ffaf292fe403a
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-05-09 12:31:23 -0400

    Now that we have streamparse on clojars, drop the clj and main from bootstrap project.clj files. #7

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -1,11 +1,11 @@
 (defproject {{ project_name }} "0.0.1-SNAPSHOT"
-  :source-paths ["clj" "topologies"]
+  :source-paths ["topologies"]
   :resource-paths ["_resources"]
   :aot :all
   :target-path "_build"
   :min-lein-version "2.0.0"
-  :main stormlocal
   :dependencies [[org.apache.storm/storm-core "0.9.1-incubating"]
+                 [com.parsely/streamparse "0.0.1-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
                  [org.clojure/tools.cli "0.3.1"]
