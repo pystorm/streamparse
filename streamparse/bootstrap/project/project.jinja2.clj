9b9d96806cca9dad917d37a6bc4aea0f1f20d9e8
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-05-09 15:26:35 -0400

    Add "-client" to JVM opts for quickstart projects.

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -4,10 +4,10 @@
   :aot :all
   :target-path "_build"
   :min-lein-version "2.0.0"
+  :jvm-opts ["-client"]
   :dependencies [[org.apache.storm/storm-core "0.9.1-incubating"]
                  [com.parsely/streamparse "0.0.1-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.4"]
-                 [org.clojure/tools.cli "0.3.1"]
-                 ]
+                 [org.clojure/tools.cli "0.3.1"]]
   )
