d29736f46f4869ed558bf0d1d0beea2ca66d3f15
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-05-09 12:28:25 -0400

    Fix run and submit_topology commands.
    Print stack traces when we run and things break and don't try to be smart about the .clj topology definition file, just accept it as a positional argument.

--- jvm/src/streamparse/commands/run.clj
+++ jvm/src/streamparse/commands/run.clj
@@ -1,34 +1,38 @@
 (ns streamparse.commands.run
-  "Submit a topology to a Storm cluster."
+  "Run a topology on a Storm LocalCluster."
   (:require [clojure.string :as string]
-            [clojure.tools.cli :refer [cli]])
+            [clojure.tools.cli :refer [cli]]
+            [clojure.stacktrace :refer [print-stack-trace]])
   (:use [backtype.storm clojure config])
   (:import  [backtype.storm LocalCluster])
   (:gen-class))
 
 
-(defn run-local! [topology-file debug sleep]
+(defn run-local! [topology-file debug run-for-secs]
   "Run the topology locally via LocalCluster. The topology definition is
   contained inside of topology-file which is assume to have a single var
   defined which contains the topology definition."
+  (println (str "Running " topology-file " for " run-for-secs))
   (try
-    (let [topology-def (load-file topology-file)
+    (let [topology-def (load-file topology-file) ; should only be a single var
           topology (apply topology (var-get topology-def))
-          topology-name (:name (meta topology-def))
+          topology-name (str (:name (meta topology-def)))
           cluster (LocalCluster.)]
       (.submitTopology cluster
                        topology-name
                        {TOPOLOGY-DEBUG debug}
                        topology)
       ;; sleep for a few seconds to let the topology run locally
-      (Thread/sleep sleep)
+      (Thread/sleep run-for-secs)
       ;; shutdown the cluster
       (.shutdown cluster))
-    (catch Exception e (println (str "Caught exception: " (.getMessage e))))))
+    (catch Exception e
+      ((println (str "Caught exception: " (.getMessage e) \newline))
+       (print-stack-trace e)))))
 
 
 (defn usage []
-  (->> ["sparsej run <topology_name>"
+  (->> ["sparsej run <topology_spec_file>"
         ""
         "Run a topology locally (no Storm cluster needed)."
         ""]
@@ -40,11 +44,11 @@
          (cli args
               ["-h" "--help" "Show this help screen." :flag true :default false]
               ["-d" "--debug" "Run with debug mode." :flag true :default false]
-              ["-s" "--sleep" "Amount of seconds to run cluser before shutting down" :default 5000 :parse-fn #(* (Integer/parseInt %) 1000)])]
+              ["-t" "--time" "Amount of seconds to run cluser before shutting down." :default 5 :parse-fn #(Integer/parseInt %)])]
     (when (or (= (count args) 0) (:help opts))
       (println (usage))
       (println banner)
       (System/exit 0))
-    ;; TODO: Should check to see if topology file exists
-    (let [topology-name (first args)]
-      (run-local! (str "topologies/" topology-name ".clj") (:debug opts) (:sleep opts)))))
+    (let [topology-spec-file (first args)
+          run-for-secs (* (:time opts) 1000)]
+      (run-local! topology-spec-file (:debug opts) run-for-secs))))
--- jvm/src/streamparse/commands/submit_topology.clj
+++ jvm/src/streamparse/commands/submit_topology.clj
@@ -1,7 +1,8 @@
 (ns streamparse.commands.submit_topology
   "Submit a topology to a Storm cluster."
   (:require [clojure.string :as string]
-            [clojure.tools.cli :refer [cli]])
+            [clojure.tools.cli :refer [cli]]
+            [clojure.stacktrace :refer [print-stack-trace]])
   (:use [backtype.storm clojure config])
   (:import  [backtype.storm StormSubmitter])
   (:gen-class))
@@ -14,15 +15,18 @@
   (try
     (let [topology-def (load-file topology-file) ; should only be a single var
           topology (apply topology (var-get topology-def))
-          topology-name (:name (meta topology-def))]
+          topology-name (str (:name (meta topology-def)))]
        (StormSubmitter/submitTopology topology-name
                                       {TOPOLOGY-DEBUG debug
                                        TOPOLOGY-WORKERS 3}
                                       topology))
-    (catch Exception e (println (str "Caught exception: " (.getMessage e))))))
+    (catch Exception e
+      ((println (str "Caught exception: " (.getMessage e) \newline))
+       (print-stack-trace e)))))
+
 
 (defn usage []
-  (->> ["sparsej submit <topology_name>"
+  (->> ["sparsej submit <topology_spec_file>"
         ""
         "Submit a topology to a remote storm cluster."
         ""]
@@ -40,6 +44,5 @@
       (println (usage))
       (println banner)
       (System/exit 0))
-    ;; TODO: Should check to see if topology file exists
-    (let [topology-name (first args)]
-      (submit-topology! (str "topologies/" topology-name ".clj") (:debug opts)))))
+    (let [topology-spec-file (first args)]
+      (submit-topology! topology-spec-file (:debug opts)))))
