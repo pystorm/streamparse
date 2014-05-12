b22e9245b955d62298333b1a8d5ef5f3ebf25952
Andrew Montalenti <andrew@parsely.com>
2014-05-12 07:24:02 -0400

    change bootstrap project.clj to exclude Storm JAR
    this makes "lein uberjar" work for now, though there is actually
    a better way to handle this situation

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -1,13 +1,13 @@
 (defproject {{ project_name }} "0.0.1-SNAPSHOT"
   :source-paths ["topologies"]
   :resource-paths ["_resources"]
-  :aot :all
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
-  :dependencies [[org.apache.storm/storm-core "0.9.1-incubating"]
-                 [com.parsely/streamparse "0.0.1-SNAPSHOT"]
-                 [org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.json "0.2.4"]
-                 [org.clojure/tools.cli "0.3.1"]]
+  :dependencies [     [org.clojure/data.json "0.2.4"]
+                      [org.clojure/tools.cli "0.3.1"]
+                      [org.apache.storm/storm-core "0.9.1-incubating"]
+                      [com.parsely/streamparse "0.0.1-SNAPSHOT"]]
+  :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
