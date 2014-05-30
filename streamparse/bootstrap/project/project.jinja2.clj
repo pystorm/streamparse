6d4f12e311e679f9720afb0ff349eb5adceb60d8
Andrew Montalenti <andrew@parsely.com>
2014-05-30 08:49:44 -0400

    bump version in project.clj bootstrap

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -7,7 +7,7 @@
   :dependencies [     [org.clojure/data.json "0.2.4"]
                       [org.clojure/tools.cli "0.3.1"]
                       [org.apache.storm/storm-core "0.9.1-incubating"]
-                      [com.parsely/streamparse "0.0.1-SNAPSHOT"]]
+                      [com.parsely/streamparse "0.0.2-SNAPSHOT"]]
   :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
