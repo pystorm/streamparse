ed8eea97a4815d0d3766ce7190d7a9d8821d9eec
Andrew Montalenti <andrew@parsely.com>
2014-06-02 07:35:42 -0400

    gitignore and bumped version

--- jvm/project.clj
+++ jvm/project.clj
@@ -1,4 +1,4 @@
-(defproject com.parsely/streamparse "0.0.2-SNAPSHOT"
+(defproject com.parsely/streamparse "0.0.3-SNAPSHOT"
   :description "Command line utilities for the Python streamparse library needed for JVM/Thrift interop with Storm clusters."
   :url "https://github.com/Parsely/streamparse"
   :mailing-list {:name "streamparse"
