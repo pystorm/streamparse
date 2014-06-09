651ff068b9de1f93c470dc10ab2a3d4281497045
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-06-08 20:39:57 -0400

    Add tasks for building docs

--- tasks.py
+++ tasks.py
@@ -1,9 +1,16 @@
 from invoke import run, task
 import os
 
+
+@task
+def build_docs():
+    run("sphinx-build doc/source doc/_build")
+
+
 @task
 def test(docs=False):
     run("nosetests --processes=-1")
+
 
 @task
 def lint():
@@ -11,15 +18,18 @@ def lint():
         if src.endswith(".py"):
             run("pyflakes streamparse/{}".format(src))
 
+
 @task(pre=["test"])
 def build(docs=False):
     run("python setup.py build")
     if docs:
-        run("sphinx-build doc/source doc/_build")
+        build_docs()
+
 
 @task
 def develop():
     run("python setup.py develop")
+
 
 @task
 def upload():
