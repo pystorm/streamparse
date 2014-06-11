284657bf849a109ecf56ba72ce6b14a52e1ff5c9
Andrew Montalenti <andrew@parsely.com>
2014-06-11 01:34:52 -0400

    bootstrap project adds clj dep

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -4,8 +4,9 @@
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
-  :dependencies [     [org.apache.storm/storm-core "0.9.1-incubating"]
-                      [com.parsely/streamparse "0.0.3-SNAPSHOT"]]
+  :dependencies  [[org.clojure/clojure "1.5.1"]
+                  [org.apache.storm/storm-core "0.9.1-incubating"]
+                  [com.parsely/streamparse "0.0.3-SNAPSHOT"]]
   :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
