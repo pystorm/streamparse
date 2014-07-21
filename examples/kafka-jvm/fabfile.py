018b432c52ac3c8e5a38b7ea314ba0c596463fcd
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-07-21 11:58:41 -0400

    Add WIP for JVM and Python topo reading from Kafka. #31

---
+++ examples/kafka-jvm/fabfile.py
@@ -0,0 +1,1 @@
+from streamparse.ext.fabric import *
---
+++ examples/kafka-jvm/project.clj
@@ -0,0 +1,13 @@
+(defproject kafka "0.0.1-SNAPSHOT"
+  :source-paths ["topologies"]
+  :resource-paths ["_resources"]
+  :target-path "_build"
+  :min-lein-version "2.0.0"
+  :jvm-opts ["-client"]
+  :dependencies [     [org.clojure/data.json "0.2.4"]
+                      [org.clojure/tools.cli "0.3.1"]
+                      [org.apache.storm/storm-core "0.9.1-incubating"]
+                      [com.parsely/streamparse "0.0.1-SNAPSHOT"]]
+  :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
+  )
