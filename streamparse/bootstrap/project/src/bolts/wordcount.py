bdbff5ea5548067713951a85b05f3818e537c8d4
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-07-24 11:30:18 -0400

    Update quickstart project to use AUTO_*

--- streamparse/bootstrap/project/src/bolts/wordcount.py
+++ streamparse/bootstrap/project/src/bolts/wordcount.py
@@ -3,7 +3,12 @@ from __future__ import absolute_import, print_function, unicode_literals
 from collections import Counter
 from streamparse.bolt import Bolt
 
+
 class WordCounter(Bolt):
+
+    AUTO_ACK = True  # automatically acknowledge tuples after process()
+    AUTO_ANCHOR = True  # automatically anchor tuples to current tuple
+    AUTO_FAIL = True  # automatically fail tuples when exceptions occur
 
     def initialize(self, conf, ctx):
         self.counts = Counter()
