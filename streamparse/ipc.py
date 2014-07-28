58dc2f4500f6af851222112a4f221093ec395770
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-07-28 15:55:42 -0400

    Typo

--- streamparse/ipc.py
+++ streamparse/ipc.py
@@ -199,7 +199,7 @@ def read_handshake():
     if log_path:
         root_log = logging.getLogger()
         max_bytes = _conf.get('stremparse.log.max_bytes') or 100000
-        backup_count = conf.get('streamparse.log.backup_count') or 10
+        backup_count = _conf.get('streamparse.log.backup_count') or 10
         log_file = ('{log_path}/streamparse_{topology_name}_{component_name}_'
                     '{task_id}_{pid}.log'
                     .format(log_path=log_path, topology_name=_topology_name,
