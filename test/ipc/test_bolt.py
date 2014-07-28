2c33700c7c539309b0a7caaf79a7a42a8eeca539
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2014-07-28 16:05:43 -0400

    Update Bolt tests to send task_ids after emits
    Also, formatting updates.

--- test/ipc/test_bolt.py
+++ test/ipc/test_bolt.py
@@ -16,72 +16,70 @@ class BoltTest(ShellComponentTestCaseMixin, unittest.TestCase):
 
     def test_echo_tuple(self):
         msg = get_message()
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         # DummyBolt should simply echo any tuple sent in to it
         self.assertEqual(res["command"], "emit")
         self.assertEqual(msg["tuple"], res["tuple"])
+        self.shell_proc.write_message([1])  # send fake task id
 
     def test_ack_tuple(self):
         msg = get_message(id="ack_me")
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "emit")
         self.assertEqual(msg["tuple"], res["tuple"])
+        self.shell_proc.write_message([1])  # send fake task id
 
         res = self.shell_proc.read_message()
         self.assertEqual(res, {"command": "ack", "id": msg["id"]})
 
     def test_fail_tuple(self):
         msg = get_message(id="fail_me")
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         self.assertEqual(res, {"command": "fail", "id": msg["id"]})
 
     def test_emit_stream(self):
         msg = get_message(id="stream-words")
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "emit")
         self.assertEqual(res["stream"], "words")
         self.assertEqual(res["tuple"], msg["tuple"])
+        self.shell_proc.write_message([1])  # send fake task id
 
     def test_emit_anchoring(self):
         msg = get_message(id="anchor|1,2,3")
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "emit")
         self.assertEqual(res["anchors"], ["1", "2", "3"])
         self.assertEqual(res["tuple"], msg["tuple"])
+        self.shell_proc.write_message([1])  # send fake task id
 
     def test_emit_direct_task(self):
         msg = get_message(id="direct_task|12")
-
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
 
+        res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "emit")
         self.assertEqual(res["task"], 12)
         self.assertEqual(res["tuple"], msg["tuple"])
 
     def test_emit_many(self):
         msg = get_message(id="emit_many")
-
         self.shell_proc.write_message(msg)
 
         for i in range(5):
             res = self.shell_proc.read_message()
             self.assertEqual(res["tuple"], msg["tuple"])
+            self.shell_proc.write_message([1])  # send fake task id
 
     # TODO: test emit_many for stream, anchoring, direct_task
 
@@ -123,6 +121,7 @@ class BoltAutoAckTest(ShellComponentTestCaseMixin, unittest.TestCase):
 
         self.assertEqual(res["command"], "emit")
         self.assertEqual(msg["tuple"], res["tuple"])
+        self.shell_proc.write_message([1]) # send fake task id
 
         res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "ack")
@@ -136,6 +135,7 @@ class BoltAutoAckTest(ShellComponentTestCaseMixin, unittest.TestCase):
             res = self.shell_proc.read_message()
             self.assertEqual(res["command"], "emit")
             self.assertEqual(res["tuple"], msg["tuple"])
+            self.shell_proc.write_message([1])  # send fake task id
 
         res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "ack")
@@ -156,6 +156,7 @@ class BoltAutoAnchorTest(ShellComponentTestCaseMixin, unittest.TestCase):
         self.assertEqual(res["command"], "emit")
         self.assertEqual(msg["tuple"], res["tuple"])
         self.assertEqual(res["anchors"], [msg["id"]])
+        self.shell_proc.write_message([1])  # send fake task id
 
     def test_emit_many_auto_anchor(self):
         msg = get_message(id="emit_many")
@@ -167,6 +168,7 @@ class BoltAutoAnchorTest(ShellComponentTestCaseMixin, unittest.TestCase):
             self.assertEqual(res["command"], "emit")
             self.assertEqual(msg["tuple"], res["tuple"])
             self.assertEqual(res["anchors"], [msg["id"]])
+            self.shell_proc.write_message([1])  # send fake task id
 
 
 class BoltAutoFailTest(ShellComponentTestCaseMixin, unittest.TestCase):
@@ -177,18 +179,20 @@ class BoltAutoFailTest(ShellComponentTestCaseMixin, unittest.TestCase):
         msg = get_message()
 
         self.shell_proc.write_message(msg)
-        res = self.shell_proc.read_message()
-
-        self.assertEqual(res["command"], "fail")
-        self.assertEqual(res["id"], msg["id"])
 
         res = self.shell_proc.read_message()
         self.assertEqual(res["command"], "log")
         self.assertIn("Exception: ", res["msg"])
 
+        res = self.shell_proc.read_message()
+        self.assertEqual(res["command"], "sync")
+
+        res = self.shell_proc.read_message()
+        self.assertEqual(res["command"], "fail")
+        self.assertEqual(res["id"], msg["id"])
+
         time.sleep(0.5)
         self.assertEqual(self.proc.poll(), 1)
-
 
 
 if __name__ == '__main__':
