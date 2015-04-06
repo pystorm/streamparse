fa64fca721f223dd998fccde87f6198bb4b64f0d
Dan Blanchard <dan@parsely.com>
2015-04-06 15:21:16 -0400

    Add name attribute to chef cookbooks for compatibility with Chef >=12

--- examples/kafka-jvm/chef/cookbooks/kafka/metadata.rb
+++ examples/kafka-jvm/chef/cookbooks/kafka/metadata.rb
@@ -1,6 +1,7 @@
 maintainer        "Webtrends, Inc."
 maintainer_email  "ivan.vonnagy@webtrends.com"
 license           "Apache 2.0"
+name              "kafka"
 description       "Intalls and configures a Kafka broker"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.20"
