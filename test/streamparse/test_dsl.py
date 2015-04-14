837a6c689989b201868587a8d640f088af996cea
Andrew Montalenti <andrew@parsely.com>
2015-04-14 16:14:02 -0400

    pep8-ify

--- test/streamparse/test_dsl.py
+++ test/streamparse/test_dsl.py
@@ -4,24 +4,26 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 class MockComponent(object):
 
-  @classmethod
-  def spec(cls,
-           name=None,
-           parallelism=1,
-           source=None,
-           group_on=None):
-      d = {
-        "parallelism": parallelism,
-        "group_on": group_on,
-        "source": source,
-        "class": cls,
-        "streams": cls.streams
-      }
-      if name is not None:
-        d["name"] = name
-      return ComponentSpec(d)
+    @classmethod
+    def spec(cls,
+             name=None,
+             parallelism=1,
+             source=None,
+             group_on=None):
+        d = {
+            "parallelism": parallelism,
+            "group_on": group_on,
+            "source": source,
+            "class": cls,
+            "streams": cls.streams
+        }
+        if name is not None:
+            d["name"] = name
+        return ComponentSpec(d)
+
 
 class WordSpout(MockComponent):
     streams = ["word"]
@@ -35,9 +37,10 @@ class WordCount(Topology):
     word_spout = WordSpout.spec(
         parallelism=2)
     word_count_bolt = WordCountBolt.spec(
-            source="word_spout",
-            group_on=Grouping.fields("word"),
-            parallelism=8)
+        source="word_spout",
+        group_on=Grouping.fields("word"),
+        parallelism=8)
+
 
 def test_wordcount_topology():
     assert len(WordCount.field_list) == 2
