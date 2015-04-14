446853af56939979aea33b2c8d8cf5ac3c696504
Curtis Vogt <curtis.vogt@gmail.com>
2015-04-14 15:20:48 -0400

    Fixed missing import and PEP8ified.

--- test/streamparse/test_dsl.py
+++ test/streamparse/test_dsl.py
@@ -1,37 +1,42 @@
-from streamparse.dsl.topology import Topology
+from streamparse.dsl.topology import Topology, ComponentSpec
+
 
 class MockComponent(object):
 
-  @classmethod
-  def spec(cls,
-           name=None,
-           parallelism=1,
-           source=None,
-           group_on=None):
-      d = {
-        "parallelism": parallelism,
-        "group_on": group_on,
-        "source": source,
-        "class": cls.__name__,
-        "streams": cls.streams
-      }
-      if name is not None:
-        d["name"] = name
-      return ComponentSpec(d)
+    @classmethod
+    def spec(cls,
+             name=None,
+             parallelism=1,
+             source=None,
+             group_on=None):
+        d = {
+            "parallelism": parallelism,
+            "group_on": group_on,
+            "source": source,
+            "class": cls.__name__,
+            "streams": cls.streams
+        }
+        if name is not None:
+            d["name"] = name
+        return ComponentSpec(d)
+
 
 class WordSpout(MockComponent):
-  streams = ["word"]
+    streams = ["word"]
+
 
 class WordCountBolt(MockComponent):
-  streams = ["word", "count"]
+    streams = ["word", "count"]
+
 
 class WordCount(Topology):
     word_spout = WordSpout.spec(
-            parallelism=2)
+        parallelism=2)
     word_count_bolt = WordCountBolt.spec(
-            source=word_spout,
-            group_on="word",
-            parallelism=8)
+        source=word_spout,
+        group_on="word",
+        parallelism=8)
+
 
 def test_wordcount_topology():
     assert len(WordCount.field_list) == 2
