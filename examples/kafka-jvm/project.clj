5ec4475f41d3dfc70a7d6a650f380df4a05cb621
Dan Blanchard <dan@parsely.com>
2015-04-23 09:29:28 -0400

    Bump up streamparse clojure version in all places that reference it.

--- examples/kafka-jvm/project.clj
+++ examples/kafka-jvm/project.clj
@@ -5,11 +5,11 @@
   :target-path "_build"
   :min-lein-version "2.0.0"
   ; :jvm-opts ["-client"]
-  :dependencies [[org.apache.storm/storm-core "0.9.2-incubating"]
-                 [org.apache.storm/storm-kafka "0.9.2-incubating" :exclusions [org.slf4j/slf4j-api]]
+  :dependencies [[org.apache.storm/storm-core "0.9.4"]
+                 [org.apache.storm/storm-kafka "0.9.4" :exclusions [org.slf4j/slf4j-api]]
                  [org.apache.kafka/kafka_2.9.2 "0.8.1.1" :exclusions [com.sun.jmx/jmxri com.sun.jdmk/jmxtools javax.jms/jms org.slf4j/slf4j-api]]
                  [org.apache.zookeeper/zookeeper "3.4.6" :exclusions [io.netty/netty org.slf4j/slf4j-api org.slf4j/slf4j-log4j12]]
-                 [com.parsely/streamparse "0.0.4-SNAPSHOT"]]
+                 [com.parsely/streamparse "0.0.5"]]
   ; :jar-exclusions     [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   ; :uberjar-exclusions [#"log4j\.properties" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   :jar-exclusions     [#"log4j\.properties" #"backtype" #"META-INF" #"meta-inf" #"\.yaml"]
