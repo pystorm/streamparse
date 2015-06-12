512339ac2272506cd66bb8a66709739783f93aae
Dan Blanchard <dan@parsely.com>
2015-06-12 16:36:37 -0400

    Clean up dynamic importing of commands in sparse.py

--- streamparse/bin/sparse.py
+++ streamparse/bin/sparse.py
@@ -1,53 +1,50 @@
+"""
+This module provides the base sparse command and a load hook for dynamically
+adding other subcommands.  The "load_suparsers" function searches for modules
+in the streamparse/bin directory that have a "subparser_hook" method. The
+"subparser_hook" accepts a the sparse subparsers object and adds it's
+subparser as needed.
+"""
+
 from __future__ import absolute_import
-import os
-import sys
-import pkgutil
+
 import argparse
 import importlib
+import os
+import pkgutil
+import sys
 
-###############################################################################
-# This module provides the base sparse command and a load hook for dynamically
-# adding other subcommands.  The "load_suparsers" function searches for modules
-# in the streamparse/bin directory that have a "subparser_hook" method. The
-# "subparser_hook" accepts a the sparse subparsers object and adds it's
-# subparser as needed.
-###############################################################################
 
 def load_suparsers(subparsers):
     """
     searches modules in streamparse/bin for a 'subparser_hook' method and calls
     the 'subparser_hook' method on the sparse subparsers object.
     """
-    for importer, package_name, _ in pkgutil.iter_modules([
-        os.path.dirname(__file__)
-    ]):
-        try:
-            mod_name = __name__.replace(
-                __file__.rsplit('/')[-1].split('.')[0], package_name
-            )
-            module   = __import__(mod_name, fromlist=['subparser_hook'])
-        except ImportError:
-            # if we can't immport it we skip trying to add the subparser
-            pass
-        # check for the subparser hook
-        if hasattr(module, 'subparser_hook'):
-            module.subparser_hook(subparsers)
+    for _, mod_name, is_pkg in pkgutil.iter_modules([os.path.dirname(__file__)]):
+        if not is_pkg and mod_name not in sys.modules:
+            module = importlib.import_module('.{}'.format(mod_name),
+                                             __package__)
+            # check for the subparser hook
+            if hasattr(module, 'subparser_hook'):
+                module.subparser_hook(subparsers)
 
 
 def main():
     """main entry point for sparse"""
-    parser = argparse.ArgumentParser(
-        prog        = "sparse",
-        description = "sparse: manage streamparse clusters.",
-        epilog      = """sparse provides a front-end to
-        streamparse, a framework for creating Python projects for running,
-        debugging, and submitting computation topologies against real-time
-        streams, using Apache Storm. It requires java and lein (Clojure
-        build tool) to be on your $PATH, and uses lein and Clojure under the
-        hood for JVM/Thrift interop.
-        """
-    )
-
+    parser = argparse.ArgumentParser(prog='sparse',
+                                     description='sparse: manage streamparse '
+                                                 'clusters.',
+                                     epilog='sparse provides a front-end to '
+                                            'streamparse, a framework for '
+                                            'creating Python projects for '
+                                            'running, debugging, and '
+                                            'submitting computation topologies '
+                                            'against real-time streams, using '
+                                            'Apache Storm. It requires java and'
+                                            ' lein (Clojure build tool) to be '
+                                            'on your $PATH, and uses lein and '
+                                            'Clojure under the hood for JVM/'
+                                            'Thrift interop.')
     subparsers = parser.add_subparsers()
     load_suparsers(subparsers)
     args = parser.parse_args()
