dea2295d8943c3a14116e31ab8d0bbbec148379d
Dan Blanchard <dan.blanchard@gmail.com>
2015-09-30 15:55:23 -0400

    Bump version to 2.1.0

--- streamparse/version.py
+++ streamparse/version.py
@@ -21,5 +21,5 @@ in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
 :organization: Parsely
 '''
 
-__version__ = '2.0.2'
+__version__ = '2.1.0'
 VERSION = tuple(int(x) for x in __version__.split('.'))
