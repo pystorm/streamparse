06c0b5cbd0aed4dc9e6a8e0066640abc14fa2b59
Dan Blanchard <dan.blanchard@gmail.com>
2015-12-17 15:33:51 -0500

    Fix import problems in run.py

--- streamparse/run.py
+++ streamparse/run.py
@@ -4,6 +4,8 @@ Helper script for starting up bolts and spouts.
 
 import argparse
 import importlib
+import os
+import sys
 
 
 def main():
@@ -15,8 +17,12 @@ def main():
                                             'streamparse.run <class name>``.')
     parser.add_argument('target_class', help='The bolt/spout class to start.')
     args = parser.parse_args()
+    # Add current directory to sys.path so imports will work
+    sys.path.append(os.getcwd())
+    # Import module
     mod_name, cls_name = args.target_class.rsplit('.', 1)
     mod = importlib.import_module(mod_name)
+    # Get class from module and run it
     cls = getattr(mod, cls_name)
     cls().run()
 
