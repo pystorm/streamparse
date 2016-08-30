99dfb3b0a44827e9c630e372c7fb355dbbba399a
Mike Sukmanowsky <mike.sukmanowsky@gmail.com>
2016-08-29 20:57:37 -0400

    Use latest Storm version for quickstart projects (#306)

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -3,8 +3,8 @@
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
-  :dependencies  [[org.apache.storm/storm-core "1.0.1"]
-                  [org.apache.storm/flux-core "1.0.1"]]
+  :dependencies  [[org.apache.storm/storm-core "1.0.2"]
+                  [org.apache.storm/flux-core "1.0.2"]]
   :jar-exclusions     [#"log4j\.properties" #"org.apache.storm" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   :uberjar-exclusions [#"log4j\.properties" #"org.apache.storm" #"backtype" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
