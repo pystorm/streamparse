74451378eb34cce0c9e1d4a652af54a64e8be7d4
Amin Alaee <mohammadamin.alaee@gmail.com>
2017-01-20 18:23:31 +0330

    read nimbus config from env variables (#347)
    * add nimbus config from env variables
    
    * make nimbus_host env variable take precedence over config.json

--- streamparse/util.py
+++ streamparse/util.py
@@ -212,14 +212,17 @@ def get_env_config(env_name=None):
 
 
 def get_nimbus_host_port(env_config):
-    """Get the Nimbus server's hostname and port from a streamparse project's
-    config file.
+    """Get the Nimbus server's hostname and port from environment variables
+    or from a streamparse project's config file.
 
     :param env_config: The project's parsed config.
     :type env_config: `dict`
 
     :returns: (host, port)
     """
+    env_config["nimbus"] = os.environ.get('STREAMPARSE_NIMBUS',
+                                          env_config["nimbus"])
+
     if not env_config["nimbus"]:
         die("No Nimbus server configured in config.json.")
 
