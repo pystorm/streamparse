80a6b761af96a685156d6d827215a1ac9e174106
Randy Watler <randy.watler@gmail.com>
2017-04-28 16:03:20 -0600

    Support 'resources' multilang Spout/Bolt module path for Storm 1.0.3+, (#362).

--- streamparse/run.py
+++ streamparse/run.py
@@ -27,10 +27,16 @@ def main():
     if len(sys.argv) == 2:
         sys.argv = [sys.argv[0]] + sys.argv[1].split()
     args = parser.parse_args()
-    # Add current directory to sys.path so imports will work
-    sys.path.append(os.getcwd())
-    # Import module
     mod_name, cls_name = args.target_class.rsplit('.', 1)
+    # Add current directory to sys.path so imports will work
+    import_path = os.getcwd()  # Storm <= 1.0.2
+    if 'resources' in next(os.walk(import_path))[1] and \
+       os.path.isfile(os.path.join(import_path,
+                                   'resources',
+                                   mod_name.replace('.', os.path.sep) + '.py')):
+        import_path = os.path.join(import_path, 'resources')  # Storm >= 1.0.3
+    sys.path.append(import_path)
+    # Import module
     mod = importlib.import_module(mod_name)
     # Get class from module and run it
     cls = getattr(mod, cls_name)
