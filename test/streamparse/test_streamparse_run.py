3c5d005e559f049c6c23c663d5cb188c28605f89
Randy Watler <randy.watler@gmail.com>
2017-05-17 09:30:21 -0600

    Simplify test arguments passed to streamparse and document, (#362).

--- test/streamparse/test_streamparse_run.py
+++ test/streamparse/test_streamparse_run.py
@@ -17,10 +17,12 @@ class StreamparseRunTests(unittest.TestCase):
 
     def test_streamparse_run_storm_1_0_2(self):
         # patch streamparse_run sys argv assuming tests run
-        # from streamparse project root, (Storm <= 1.0.2 case)
+        # from streamparse project root, (the Storm <= 1.0.2 case):
+        # target test run module specifying absolute import path
+        # including 'test' folder
         sys.argv = ['streamparse_run',
-                    'test.'
-                    'streamparse_run.streamparse_run_target.StreamparseRunTarget']
+                    'test.streamparse_run.streamparse_run_target.StreamparseRunTarget']
+        # invoke run module main
         StreamparseRunTests.run_target_invoked = False
         StreamparseRunTests.run_target_invoked_serializer = None
         run.main()
@@ -29,12 +31,15 @@ class StreamparseRunTests(unittest.TestCase):
 
     def test_streamparse_run_storm_1_0_3(self):
         # patch streamparse_run resources path and sys argv
-        # assuming tests run from streamparse project root,
-        # (Storm >= 1.0.3 case)
+        # assuming tests run from streamparse project root
+        # with 'extra' resources folder, (Storm >= 1.0.3 case):
+        # target test run module specifying relative import
+        # path within 'test' resources folder; also test
+        # serializer option
         run.RESOURCES_PATH = 'test'
         sys.argv = ['streamparse_run',
-                    'streamparse_run.streamparse_run_target.StreamparseRunTarget '
-                    '--serializer=msgpack']
+                    'streamparse_run.streamparse_run_target.StreamparseRunTarget --serializer=msgpack']
+        # invoke run module main
         StreamparseRunTests.run_target_invoked = False
         StreamparseRunTests.run_target_invoked_serializer = None
         run.main()
