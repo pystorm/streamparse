d1db7d7953c7304ee190c69863a5a1aec9e9b197
Dan Blanchard <dan.blanchard@gmail.com>
2017-05-19 16:38:24 -0400

    Bump default storm version for bootstrap up to 1.1.0

--- streamparse/bootstrap/project/project.jinja2.clj
+++ streamparse/bootstrap/project/project.jinja2.clj
@@ -3,8 +3,8 @@
   :target-path "_build"
   :min-lein-version "2.0.0"
   :jvm-opts ["-client"]
-  :dependencies  [[org.apache.storm/storm-core "1.0.2"]
-                  [org.apache.storm/flux-core "1.0.2"]]
+  :dependencies  [[org.apache.storm/storm-core "1.1.0"]
+                  [org.apache.storm/flux-core "1.1.0"]]
   :jar-exclusions     [#"log4j\.properties" #"org\.apache\.storm\.(?!flux)" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   :uberjar-exclusions [#"log4j\.properties" #"org\.apache\.storm\.(?!flux)" #"trident" #"META-INF" #"meta-inf" #"\.yaml"]
   )
