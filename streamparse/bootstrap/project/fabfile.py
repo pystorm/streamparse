aee5d931be2f0536ae23549ae80ff910eff5f469
Jason Yan <tailofthesun@gmail.com>
2018-06-15 13:58:07 -0700

    Added new `options` argument to pre/post-submit hooks.
    Fixes #401 for newly bootstrapped projects.

--- streamparse/bootstrap/project/fabfile.py
+++ streamparse/bootstrap/project/fabfile.py
@@ -1,10 +1,10 @@
-def pre_submit(topology_name, env_name, env_config):
+def pre_submit(topology_name, env_name, env_config, options):
     """Override this function to perform custom actions prior to topology
     submission. No SSH tunnels will be active when this function is called."""
     pass
 
 
-def post_submit(topo_name, env_name, env_config):
+def post_submit(topo_name, env_name, env_config, options):
     """Override this function to perform custom actions after topology
     submission. Note that the SSH tunnel to Nimbus will still be active
     when this function is called."""
