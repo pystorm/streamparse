43234851e5db2cb218591ab7a9342dc511f2d800
Dan Blanchard <dan.blanchard@gmail.com>
2019-04-09 10:40:44 -0400

    Update copyright dates

--- streamparse/version.py
+++ streamparse/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2014-2017 Parsely, Inc.
+# Copyright 2014-2019 Parsely, Inc.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
