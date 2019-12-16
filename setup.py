da8b96c4ed9b8d6dd77083f539f017bb97f56173
Dan Blanchard <dan.blanchard@gmail.com>
2019-12-16 17:06:27 -0500

    Python 3.4 is past EOL, so bye bye

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
